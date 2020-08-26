3f420ec1bcd0b5b32ca42b3638e489825b539044
Yuri Alves <yuripiresalves@gmail.com>
2020-08-26 18:37:10 -0300

    configure nunjucks

--- modulo03/server.js
+++ modulo03/server.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", (req, res) => {
+    return res.render('about')
+})
+
+server.get("/portfolio", (req, res) => {
+    return res.render('portfolio')
+})
 
 server.listen(5000, () => {
     console.log('pai ta on')
