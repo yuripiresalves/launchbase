f853c451f29addec3641ac84370edf2665939c7f
Yuri Alves <yuripiresalves@gmail.com>
2020-08-27 13:39:02 -0300

    created and configured server

---
+++ modulo03/desafio/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjukcs = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjukcs.configure('views', {
+    express: server
+})
+
+server.get("/", (req, res) => {
+    return res.render("courses")
+})
+
+server.get("/about", (req, res) => {
+    return res.render("about")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found")
+  })
+
+server.listen(5000, () => {
+    console.log('pai ta on')
+})
