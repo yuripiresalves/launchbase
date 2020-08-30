a538fb04950c332013b1c2be0653de4e99075df0
Yuri Alves <yuripiresalves@gmail.com>
2020-08-29 23:22:55 -0300

    added and configured video page

--- modulo03/server.js
+++ modulo03/server.js
@@ -10,7 +10,8 @@ server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server,
-    autoescape: false
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", (req, res) => {
@@ -30,6 +31,20 @@ server.get("/", (req, res) => {
 
 server.get("/portfolio", (req, res) => {
     return res.render('portfolio', { items: videos })
+})
+
+server.get("/video", (req, res) => {
+    const id = req.query.id
+
+    const video = videos.find((video) => {
+        return video.id === id
+    })
+
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render('video', { item: video })
 })
 
 server.listen(5000, () => {
