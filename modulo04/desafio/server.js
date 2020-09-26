792b242252a17c15bd0ad44b7aaa9635d3475913
Yuri Alves <yuripiresalves@gmail.com>
2020-09-26 18:54:24 -0300

    configure urlencoded (req.body)

--- modulo04/desafio/server.js
+++ modulo04/desafio/server.js
@@ -1,9 +1,11 @@
+const { urlencoded } = require('express')
 const express = require('express')
 const nunjukcs = require('nunjucks')
 const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({ extended:true }))
 server.use(express.static('public'))
 server.use(routes)
 
