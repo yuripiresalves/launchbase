6f05bfb502f8717ad2818a75bf23a570b21768d7
Yuri Alves <yuripiresalves@gmail.com>
2020-09-28 11:26:42 -0300

    create show instructor page logic

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
+// Show
+exports.show = function (req, res) {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+    }
+
+    return res.render('instructors/show', { instructor })
+}
+
 // Create
 exports.post = function (req, res) {
 
