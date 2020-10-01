ca8b0e45d622d6bfcbeda4b961f693c304b2e74d
Yuri Alves <yuripiresalves@gmail.com>
2020-10-01 16:37:36 -0300

    add date function

--- modulo04/desafio/utils.js
+++ modulo04/desafio/utils.js
@@ -30,5 +30,15 @@ module.exports = {
             default:
                 return "Error!"
         }
+    },
+
+    date: (timestamp) => {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return `${year}-${month}-${day}`
     }
 }
