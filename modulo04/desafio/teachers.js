52aff72dcd4328948f007bc74b34d4b5ceacbb75
Yuri Alves <yuripiresalves@gmail.com>
2020-09-30 23:27:49 -0300

    add show page

--- modulo04/desafio/teachers.js
+++ modulo04/desafio/teachers.js
@@ -1,5 +1,29 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, graduation } = require('./utils')
+
+// Show
+exports.show = (req, res) => {
+
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find((teacher) => {
+        return teacher.id == id
+    })
+
+    if (!foundTeacher) return res.send("Teacher not found!")
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        graduation: graduation(foundTeacher.graduation),
+        classes: foundTeacher.classes.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at)
+    }
+
+    return res.render('teachers/show', { teacher })
+}
+
 // Create
 exports.post = (req, res) => {
 
@@ -12,7 +36,7 @@ exports.post = (req, res) => {
         }
     }
 
-    let { avatar_url, name, birth, escolaridade, classes_type, classes } = req.body
+    let { avatar_url, name, birth, graduation, classes_type, classes } = req.body
 
     birth = Date.parse(birth)
     const id = Number(data.teachers.length + 1)
@@ -23,7 +47,7 @@ exports.post = (req, res) => {
         avatar_url,
         name,
         birth,
-        escolaridade,
+        graduation,
         classes_type,
         classes,
         created_at
@@ -32,7 +56,7 @@ exports.post = (req, res) => {
     fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if(err) return res.send("Write file error!")
 
-        return res.redirect('/teachers')
+        return res.redirect(`/teachers/${id}`)
     })
 
 }
