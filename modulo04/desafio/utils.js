a8a1bd9cb240d46cd7308c3c8013d5cfb3b6137d
Yuri Alves <yuripiresalves@gmail.com>
2020-09-30 23:16:51 -0300

    add utils

---
+++ modulo04/desafio/utils.js
@@ -0,0 +1,34 @@
+module.exports = {
+    age: (timestamp) => {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+            age = age - 1
+        }
+
+        return age
+    },
+
+    graduation: (value) => {
+        switch (value) {
+            case "medio":
+                return "Ensino Médio Completo"
+            
+            case "superior":
+                return "Ensino Superior Completo"
+            
+            case "mestrado":
+                return "Mestrado"
+            
+            case "doutorado":
+                return "Doutorado"
+
+            default:
+                return "Error!"
+        }
+    }
+}
