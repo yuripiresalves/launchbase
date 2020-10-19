dbb0dbe60f1d32726bb86993623013481013c065
Yuri Alves <yuripiresalves@gmail.com>
2020-10-19 17:39:00 -0300

    add grade function and update date function

--- modulo04/desafio/utils.js
+++ modulo04/desafio/utils.js
@@ -39,6 +39,43 @@ module.exports = {
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         const day = `0${date.getUTCDate()}`.slice(-2)
 
-        return `${year}-${month}-${day}`
+        return {
+            day,
+            month,
+            year,
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`,
+        }
+    },
+
+    grade: (value) => {
+        switch (value) {
+            case "5EF":
+                return "5º Ano do Ensino Fundamental"
+            
+            case "6EF":
+                return "6º Ano do Ensino Fundamental"
+            
+            case "7EF":
+                return "7º Ano do Ensino Fundamental"
+            
+            case "8EF":
+                return "8º Ano do Ensino Fundamental"
+            
+            case "9EF":
+                return "9º Ano do Ensino Fundamental"
+            
+            case "1EM":
+                return "1º Ano do Ensino Médio"
+            
+            case "2EM":
+                return "2º Ano do Ensino Médio"
+            
+            case "3EM":
+                return "3º Ano do Ensino Médio"
+
+            default:
+                return "Error!"
+        }
     }
 }
