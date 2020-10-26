5467d5487baa2b1b31120945fa76525736929563
Yuri Alves <yuripiresalves@gmail.com>
2020-10-26 15:10:34 -0300

    refactoring directories

---
+++ modulo05/src/app/controllers/instructors.js
@@ -0,0 +1,43 @@
+const { age, date } = require('../../lib/utils')
+
+module.exports = {
+  index(req, res) {
+    return res.render('instructors/index')
+  },
+  create(req, res) {
+    return res.render('instructors/create')
+  },
+  post(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (const key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Preencha todos os campos!")
+      }
+    }
+
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (const key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Preencha todos os campos!")
+      }
+    }
+
+    return
+  },
+  delete(req, res) {
+    return
+  },
+}
