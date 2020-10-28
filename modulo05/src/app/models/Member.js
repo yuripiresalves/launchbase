225a12f43d9c7cab5813ded88bdb0b4962497009
Yuri Alves <yuripiresalves@gmail.com>
2020-10-28 17:25:10 -0300

    add instructor option

--- modulo05/src/app/models/Member.js
+++ modulo05/src/app/models/Member.js
@@ -19,8 +19,9 @@ module.exports = {
         birth,
         blood,
         weight,
-        height
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        height,
+        instructor_id
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id
     `
 
@@ -32,7 +33,8 @@ module.exports = {
       date(data.birth).iso,
       data.blood,
       data.weight,
-      data.height
+      data.height,
+      data.instructor
     ]
 
     db.query(query, values, function (err, results) {
@@ -43,9 +45,10 @@ module.exports = {
   },
   find(id, callback) {
     db.query(`
-      SELECT * 
-      FROM members 
-      WHERE id = $1`, [id], function (err, results) {
+      SELECT members.*, instructors.name AS instructor_name 
+      FROM members
+      LEFT JOIN instructors ON (members.instructor_id = instructors.id) 
+      WHERE members.id = $1`, [id], function (err, results) {
         if (err) throw `Database Error! ${err}`
 
         callback(results.rows[0])
@@ -61,8 +64,9 @@ module.exports = {
       email=($5),
       blood=($6),
       weight=($7),
-      height=($8)
-    WHERE id = $9
+      height=($8),
+      instructor_id=($9)
+    WHERE id = $10
     `
 
     const values = [
@@ -74,6 +78,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
       data.id
     ]
 
@@ -88,6 +93,13 @@ module.exports = {
       if (err) throw `Database Error! ${err}`
 
       return callback()
+    })
+  },
+  instructorsSelectOptions(callback) {
+    db.query(`SELECT name, id FROM instructors`, function (err, results) {
+      if (err) throw `Database Error! ${err}`
+
+      callback(results.rows)
     })
   }
 }
