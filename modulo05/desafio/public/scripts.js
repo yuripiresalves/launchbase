9d2e8c685f40dc05b9eb0140202b9ed0d18bda13
Yuri Alves <yuripiresalves@gmail.com>
2020-11-01 22:25:30 -0300

    add pagination

--- modulo05/desafio/public/scripts.js
+++ modulo05/desafio/public/scripts.js
@@ -2,7 +2,64 @@ const currentPage = location.pathname
 const menuItems = document.querySelectorAll('header .links a')
 
 for (item of menuItems) {
-    if (currentPage.includes(item.getAttribute('href'))) {
-        item.classList.add('active')
+  if (currentPage.includes(item.getAttribute('href'))) {
+    item.classList.add('active')
+  }
+}
+
+function paginate(totalPages, selectedPage) {
+  let pages = [],
+    oldPage
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPages = currentPage == 1 || currentPage == 2 || currentPage == totalPages || currentPage == totalPages - 1
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 1
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
+
+    if (firstAndLastPages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push(">>>")
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1)
+      }
+
+      pages.push(currentPage)
+
+      oldPage = currentPage
     }
+  }
+  return pages
+}
+
+function createPagination(pagination) {
+  const filter = pagination.dataset.filter
+  const page = +pagination.dataset.page
+  const total = +pagination.dataset.total
+  const pages = paginate(total, page)
+
+  let elements = ""
+
+  for (let page of pages) {
+    if (String(page).includes(">>>")) {
+      elements += `<span>${page}</span>`
+
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+      }
+    }
+  }
+
+  pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+  createPagination(pagination)
 }
--- modulo05/desafio/src/app/models/Student.js
+++ modulo05/desafio/src/app/models/Student.js
@@ -95,5 +95,40 @@ module.exports = {
 
       callback(results.rows)
     })
+  },
+  paginate(params) {
+    const { filter, limit, offset, callback } = params
+
+    let query = "",
+        filterQuery = "",
+        totalQuery = `(
+          SELECT count(*) FROM students
+        ) AS total`
+
+    if (filter) {
+
+      filterQuery = `
+      WHERE students.name ILIKE '%${filter}%'
+      OR students.email ILIKE '%${filter}%'
+      `
+
+      totalQuery = `(
+        SELECT count(*) FROM students
+        ${filterQuery}
+      ) AS total`
+    }
+
+    query = `
+    SELECT students.*, ${totalQuery}
+    FROM students
+    ${filterQuery}
+    LIMIT $1 OFFSET $2
+    `
+
+    db.query(query, [limit, offset], (err, results) => {
+      if (err) throw `Database Error! ${err}`
+
+      callback(results.rows)
+    })
   }
 }
